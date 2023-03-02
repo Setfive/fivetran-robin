edc54cd80b8c61d9cb39c652926c935ddccfe959
Ashish Datta <ashish@setfive.com>
2023-03-02 17:46:01 -0500

    Wiring up actual API fetch

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
 import * as AWS from 'aws-sdk';
+import moment from 'moment';
+import axios from 'axios';
 
 const s3 = new AWS.S3();
 
 interface IFivetranRequest {
-    state: {cursor: string};
-    secrets: {apiKey: string};
+    state: {cursor: string | undefined};
+    secrets: {apiKey: string, organizationId: string, startDate?: string};
     bucket: string;
     file: string;
 }
@@ -46,7 +48,97 @@ interface IS3CopyResult {
     error?: string;
 }
 
+interface IRobinAnalyticsResult {
+    meta: {
+        status_code: number,
+        status: 'ACCEPTED' | 'BAD_REQUEST' | 'NOT_FOUND',
+        message: string,
+        more_info: unknown,
+        errors: string[]
+    },
+    data: {
+        export_id: string,
+        report_id: string,
+    }
+}
+
+interface IDoFetchResult {
+    success: boolean;
+    error?: string;
+    data?: string;
+}
+
+async function doFetchRecords(request: IFivetranRequest): Promise<IDoFetchResult> {
+    let startDate = '';
+    let endDate = '';
+    let cursorDate: moment.Moment;
+    const now = moment();
+
+    if(request.state.cursor) {
+        cursorDate = moment(request.state.cursor).startOf('day');
+        startDate = request.state.cursor;
+    }else{
+        if(request.secrets.startDate) {
+            cursorDate = moment(request.secrets.startDate).startOf('day');
+        }else{
+            cursorDate = moment('2008-01-01T00:00:00Z');
+        }
+
+        startDate = cursorDate.format();
+    }
+
+    if(now.diff(cursorDate, 'days') > 185) {
+        const cursorEndDate = cursorDate.add(185, 'day').endOf('day');
+        endDate = cursorEndDate.format();
+    }else{
+        endDate = now.format();
+    }
+
+    console.log(`startDate = ${startDate}, endDate = ${endDate}`);
+
+    const url = `https://api.robinpowered.com/v1.0/insights/exports/organizations/${request.secrets.organizationId}/spaces`;
+    const payload = {from: startDate, to: endDate};
+    const apiResult = await axios.post<IRobinAnalyticsResult>(url, payload, {headers: {Authorization: `Access-Token ${request.secrets.apiKey}`, 'Content-Type': 'application/json'}});
+
+    if(apiResult.data.meta.status !== 'ACCEPTED') {
+        return {
+            success: false,
+            error: `${apiResult.data.meta.status}: ${apiResult.data.meta.message}`
+        };
+    }
+
+    const exportId = apiResult.data.data.export_id;
+    let sleepTime = 1;
+    let numAttempts = 0;
+    let data = '';
+
+    do {
+        await sleep(sleepTime);
+        const url = `https://api.robinpowered.com/v1.0/insights/exports/${exportId}`;
+        const apiResult = await axios.get<IRobinAnalyticsResult>(url, {headers: {Authorization: `Access-Token ${request.secrets.apiKey}`, 'Content-Type': 'application/json'}});
+
+        sleepTime = sleepTime * 2;
+        numAttempts += 1;
+    }while(numAttempts < 10);
+
+}
+
+async function sleep(sleepTimeMs: number) {
+    return new Promise<void>((resolve, reject) => setTimeout(resolve, sleepTimeMs * 1000))
+}
+
+doFetchRecords({
+    "state": {"cursor": "2020-01-01T00:00:00Z"},
+    "secrets": {apiKey: `${process.env.ROBIN_KEY}`, organizationId: '34', startDate: "2020-01-01T00:00:00Z"},
+    "bucket": "setfive-robin-sync",
+    "file": "test2.csv",
+})
+
 export const handler = async (request: IFivetranRequest, context: Context): Promise<IFivetranResult | IFivetranError> => {
+
+    // TODO: Verify the required secrets are present and error if not
+
+    console.log(`Cursor: ${request.state.cursor}, Destination: s3://${request.bucket}/${request.file}`);
 
     let fetchResult: IFetchResponse;
 
@@ -60,6 +152,8 @@ export const handler = async (request: IFivetranRequest, context: Context): Prom
             stackTrace: new Error().stack,
         }
     }
+
+    console.log(`Received: Inserts = ${fetchResult.s3Data.insert.transactions.length}, Deletes =  ${fetchResult.s3Data.delete.transactions.length}`);
 
     const s3Result = await copyToS3(request.bucket, request.file, JSON.stringify(fetchResult.s3Data));
 
